7bc31e6703e10fd89796438faa74002d8d3d6b5c
bashantad <dahal_bsd@yahoo.com>
2013-12-12 21:58:25 +1100

    configuration stuff added

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,27 +1,51 @@
+require "bundler/capistrano"
+
+server "106.186.30.230", :web, :app, :db, primary: true
+
 set :application, "test_deploy"
-set :repository,  "git@github.com:bashantad/test-deploy.git"
-set :deploy_via, :copy
-set :keep_releases, 3
-set :deploy_to, "/home/deployer/public"
-# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :user, "bash"
+set :deploy_to, "/home/#{user}/apps/#{application}"
+set :deploy_via, :remote_cache
+set :use_sudo, false
 
-role :web, "nginx"                          # Your HTTP server, Apache/etc
-role :app, "www.chaseuni.com"                          # This may be the same as your `Web` server
-role :db,  "www.chaseuni.com", :primary => true # This is where Rails migrations will run
-role :db,  "www.chaseuni.com"
+set :scm, "git"
+set :repository, "git@github.com:ryanb/#{application}.git"
+set :branch, "master"
 
-# if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
+after "deploy", "deploy:cleanup" # keep only the last 5 releases
 
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :deploy do
+  %w[start stop restart].each do |command|
+    desc "#{command} unicorn server"
+    task command, roles: :app, except: {no_release: true} do
+      run "/etc/init.d/unicorn_#{application} #{command}"
+    end
+  end
+
+  task :setup_config, roles: :app do
+    sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
+    sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
+    run "mkdir -p #{shared_path}/config"
+    put File.read("config/database.example.yml"), "#{shared_path}/config/database.yml"
+    puts "Now edit the config files in #{shared_path}."
+  end
+  after "deploy:setup", "deploy:setup_config"
+
+  task :symlink_config, roles: :app do
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+  end
+  after "deploy:finalize_update", "deploy:symlink_config"
+
+  desc "Make sure local git is in sync with remote."
+  task :check_revision, roles: :web do
+    unless `git rev-parse HEAD` == `git rev-parse origin/master`
+      puts "WARNING: HEAD is not the same as origin/master"
+      puts "Run `git push` to sync changes."
+      exit
+    end
+  end
+  before "deploy", "deploy:check_revision"
+end
